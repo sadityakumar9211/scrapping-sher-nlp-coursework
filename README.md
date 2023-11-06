a63b9f0e997f7dccb2b395dbaf32c456102a079e
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2023-11-07 02:01:14 +0530

    scrapping completed
    Signed-off-by: Aditya Kumar Singh <sadityakumar9211@gmail.com>

---
+++ README.md
@@ -0,0 +1,33 @@
+# 
+
+## Web Links: 
+
+https://www.rekhta.org/tags/intezar-shayari/couplets
+
+https://www.rekhta.org/tags/sad-shayari/couplets
+
+https://www.rekhta.org/tags/tamanna-shayari/couplets
+
+https://www.rekhta.org/tags/romantic-shayari/couplets
+
+https://www.rekhta.org/tags/judai-shayari/couplets?lang=hi
+
+https://www.rekhta.org/tags/ishq-shayari/couplets?lang=hi
+
+https://www.rekhta.org/tags/deshbhakti-shayari/couplets
+
+https://www.rekhta.org/tags/bachpan-shayari/couplets
+
+https://www.rekhta.org/tags/peace-shayari/couplets
+
+https://www.rekhta.org/tags/husn-shayri/couplets
+
+
+
+tags = ["intezar", "sad", "tamanna", "romantic", "judai", "ishq", "deshbhakti", "bachpan", "peace", "husn"]
+
+
+genericUrl = "https://www.rekhta.org/tags/{tag}-shayari/couplets?lang=hi"
+
+## Structure of HTML page: 
+
---
+++ script.py
@@ -0,0 +1,50 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+# Define the list of tags and the generic URL
+tags = ["intezar", "sad", "tamanna", "romantic", "judai", "ishq", "deshbhakti", "bachpan", "peace", "husn"]
+genericUrl = "https://www.rekhta.org/tags/{tag}-shayari/couplets?lang=hi"
+
+# Function to scrape and save Shayari data to CSV files
+def scrape_and_save_shayari():
+    sher_data = {}  # Dictionary to store sher and their associated tags
+
+    for tag in tags:
+        url = genericUrl.format(tag=tag)
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            sher_section = soup.find_all('div', class_='sherSection')
+            for sher in sher_section:
+                sher_lines = sher.find_all('p')
+                if len(sher_lines) >= 2:
+                    line1 = ' '.join([span.text for span in sher_lines[0].find_all('span')]).strip()
+                    line2 = ' '.join([span.text for span in sher_lines[1].find_all('span')]).strip()
+                    shayari_text = f"{line1} | {line2}"
+
+                    if shayari_text not in sher_data:
+                        sher_data[shayari_text] = [tag]
+                    else:
+                        sher_data[shayari_text].append(tag)
+
+    # Create the original CSV file with unique sher
+    with open('shayari.csv', 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['sher', 'tags'])
+
+        for sher, tag_list in sher_data.items():
+            if len(tag_list) == 1:
+                writer.writerow([sher, ', '.join(tag_list)])
+
+    # Create a separate CSV file for duplicate shers
+    with open('duplicate_shayari.csv', 'w', newline='', encoding='utf-8') as duplicate_csvfile:
+        duplicate_writer = csv.writer(duplicate_csvfile)
+        duplicate_writer.writerow(['sher', 'tags'])
+
+        for sher, tag_list in sher_data.items():
+            if len(tag_list) > 1:
+                duplicate_writer.writerow([sher, ', '.join(tag_list)])
+
+# Call the function to scrape and save Shayari
+scrape_and_save_shayari()
